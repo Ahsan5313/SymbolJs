b2781424707e29bbc4c7f20862fe54cadf28f397
Ahsan <ahsanulhuq17@gmail.com>
2022-07-06 23:22:20 +0600

    new add

--- js/main.js
+++ js/main.js
@@ -113,53 +113,67 @@
 //     return value;
 // })
 
-const fistInput = document.getElementById('fistInput');
-const secondInput = document.getElementById('secondInput');
-const result = document.getElementById('result');
-const resultShow = document.getElementById('resultShow');
+// const fistInput = document.getElementById('fistInput');
+// const secondInput = document.getElementById('secondInput');
+// const result = document.getElementById('result');
+// const resultShow = document.getElementById('resultShow');
 
-result.addEventListener('click', function(e) {
+// result.addEventListener('click', function(e) {
 
-     e.preventDefault();
-    const fistInputPaseInt = parseInt(fistInput.value);
-    const secondInputPaseInt = parseInt(secondInput.value);
+//      e.preventDefault();
+//     const fistInputPaseInt = parseInt(fistInput.value);
+//     const secondInputPaseInt = parseInt(secondInput.value);
 
-     if(fistInputPaseInt === '' && secondInputPaseInt == ''){
+//      if(fistInputPaseInt === '' && secondInputPaseInt == ''){
 
-       console.log('Hello world')
-     }else{
+//        console.log('Hello world')
+//      }else{
 
-           for (let i = 1; i <= secondInputPaseInt; i++) {
-            const result = i * fistInputPaseInt;
-            const second = i ;
+//            for (let i = 1; i <= secondInputPaseInt; i++) {
+//             const result = i * fistInputPaseInt;
+//             const second = i ;
 
-            createElement(fistInputPaseInt, second, result)
-       }
+//             createElement(fistInputPaseInt, second, result)
+//        }
     
       
-   }    
-});
+//    }    
+// });
 
-function createElement(tdOne, tdTwo, tdThree){
+// function createElement(tdOne, tdTwo, tdThree){
      
-     const tr = document.createElement('tr');
-     const td1 = document.createElement('td');
-     const td2 = document.createElement('td');
-     const td3 = document.createElement('td');
+//      const tr = document.createElement('tr');
+//      const td1 = document.createElement('td');
+//      const td2 = document.createElement('td');
+//      const td3 = document.createElement('td');
 
-     const td1Value = document.createTextNode(tdOne);
-     const td2Value = document.createTextNode(tdTwo);
-     const td3Value = document.createTextNode(tdThree);
+//      const td1Value = document.createTextNode(tdOne);
+//      const td2Value = document.createTextNode(tdTwo);
+//      const td3Value = document.createTextNode(tdThree);
      
-     td1.appendChild(td1Value);
-     td2.appendChild(td2Value);
-     td3.appendChild(td3Value);
+//      td1.appendChild(td1Value);
+//      td2.appendChild(td2Value);
+//      td3.appendChild(td3Value);
 
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
+//     tr.appendChild(td1);
+//     tr.appendChild(td2);
+//     tr.appendChild(td3);
 
-    resultShow.append(tr)
+//     resultShow.append(tr)
     
 
-} 
+// } 
+
+// function newWord(str){
+
+//      let newWord = '';
+
+//      for(let i = 2 ; i < str.length; i++){
+
+//           newWord += str[i];
+//      }
+
+//     return newWord; 
+
+// }
+// console.log(newWord('object'))
